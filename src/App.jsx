9143b5657d537f69fa790997c980de772e44eb0d
= <nicolescayambas@gmail.com>
2024-06-12 19:47:35 +0800

    basic funcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Calendar from 'react-calendar'
 import './App.css'
+import { haikus } from './data';
+// import 'react-calendar/dist/Calendar.css';
+const today = new Date();
+const firstDay = new Date('2024-05-12');
+
+const noHaikuWritten = 'Didn\'t write any for this day :(';
+
+const NewlineText = (props) => {
+  const text = props.text;
+  const newText = text.split('\n').map(str => <p>{str}</p>);
+  
+  return newText;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState(new Date());
+  const [selectedHaiku, setSelectedHaiku] = useState('Select a date :)');
+
+  const onClickDay = (value, event) => {
+    const foundHaiku = haikus.find((haiku) => haiku.date == value.toLocaleDateString());
+    let finalHaiku = foundHaiku?.haiku ?? noHaikuWritten;
+    if(foundHaiku?.isSpg) {
+      const password = prompt("Password");
+      const correctPassword = password === 'dinosaur<3';
+      if(!correctPassword) {
+        finalHaiku = "This is not meant for you.";
+      }
+    }
+    setSelectedHaiku(finalHaiku);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='calendar-container'>
+      <Calendar onClickDay={onClickDay} value={value} maxDate={today} minDate={firstDay}/>
+    </div>
+    <div className='haiku'>
+      <NewlineText text={selectedHaiku} />
+    </div>
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,132 @@
+export const haikus = [
+    {
+        date: '5/12/2024',
+        haiku: `a day of answers\nto years of questions and doubts\nour story begins\n\nit was mothers day\ni recall pink carnations\nin your pretty hands`,
+    },
+    {
+        date: '5/13/2024',
+        haiku: `mondays always suck\nyou asked if i've eaten yet\nmondays do not suck\n\ni put on twilight\nit's your comfort movie\nwhat else do you like?\ni wanna know everything`,
+    },
+    {
+        date: '5/14/2024',
+        haiku: `i woke up early\njust to say good morning love\ni could finally say it\n\ni watched twilight 4\nthinking it was twilight 2\ni spoiled myself bad\ni'm glad it made us laugh`,
+    },
+    {
+        date: '5/15/2024',
+        haiku: `what a nice morning\nyou told me about your dream\ni was teaching you poetry\nwe had a golden retriever\ni'm chill if you want to make it real`,
+    },
+    {
+        date: '5/16/2024',
+        haiku: `we planned our first date\ni was nervous\n'i hope she likes me'`,
+    },
+    {
+        date: '5/17/2024',
+        haiku: `🕳️🐢🦖💨\nyou're a dinosaur\ni'm a turtle\nif you fart, we fall into the void`,
+    },
+    {
+        date: '5/18/2024',
+        haiku: `'pagkain na ako ngayon 👀'\n'patikim garod'\n'soon'\nit was midnight and i died`,
+    },
+    {
+        date: '5/19/2024',
+        haiku: `san juan\n\nour first date :)\nit was cloudy at the beach\nwe were playing lego\nand you were beside me\n\nnight came and told us to go home\nbut i didn't want to\nit was perfect where we were\n\non the way back, you held my hand\ni thought of that all week\nwas i ever this happy?`,
+    },
+    {
+        date: '5/20/2024',
+        haiku: `a b c d e f g h i j k l m n o p q r s t u v w x y z\ni l y`,
+    },
+    {
+        date: '5/21/2024',
+        haiku: `you bought a lot of stuff\ni got inspired, i wanted to splurge too\ni wanted a notebook\na coco martin one\nto fill with poems for you`,
+    },
+    {
+        date: '5/22/2024',
+        haiku: `your date with naya\nshe's my favorite friend of yours\nyou were cold\nif i were there, i'd have been your jacket`,
+    },
+    {
+        date: '5/23/2024',
+        haiku: `witwit hung himself in the early morning\nyour family asked why you would buy green pringles\nyou danced to jojo siwa\na weird day for you\ni love hearing about your weird days`,
+    },
+    {
+        date: '5/24/2024',
+        haiku: `kalokohan na lahat\nkasi sayo lang ako seryoso`,
+    },
+    {
+        date: '5/25/2024',
+        haiku: `i met my friends day\nyour haircut day\ncity lights day\ni met your friends day\n\nyou sent me home\nyou were upset about how \ni didnt want you to send me home\ni was sorry for not taking it seriously\n\ni'll take care if you take care`,
+    },
+    {
+        date: '5/26/2024',
+        haiku: `dinner with our friends\ni missed them, but i was missing you more\neven though you were there`,
+    },
+    {
+        date: '5/27/2024',
+        haiku: `you were with your worms more than with me :(`,
+    },
+    {
+        date: '5/28/2024',
+        haiku: `i called you when you were cleaning your room\ni was working but i kept stealing glances at you\nhow did i get so lucky?\n\nthen i ruined the day by being jealous\nyou didn't say good night :(\nso i waited for your good morning\ni didn't sleep`,
+    },
+    {
+        date: '5/29/2024',
+        haiku: `sometimes when i'm drawing up plans for the future\ni leave a space in there somewhere\nit's in the shape of you\nis it too soon to be this sure?`,
+    },
+    {
+        date: '5/30/2024',
+        haiku: `i had a dream i saw a meteor shower\ni messaged you about it\nevent the shooting stars in my dreams know\nhow much i want you`,
+    },
+    {
+        date: '5/31/2024',
+        haiku: `i wait another day\nmy life has become a countdown\nto when i can hold your hand again\none more day...`,
+    },
+    {
+        date: '6/1/2024',
+        haiku: `pride month starts\nand you're in my place\nyour hands are finally in mine\nim happy right here\ntracing i love you on your fingers\n\nbefore you leave, you kiss me on the cheek\ni smile right after, like you planted it there\nas the taxi goes, i start to miss you again`,
+    },
+    {
+        date: '6/2/2024',
+        haiku: `i miss you bad\ncome back :(`,
+    },
+    {
+        date: '6/3/2024',
+        haiku: `i ask you who you've been with\neven with faceless people, i get jealous\neverybody wants you, and i feel like everybody\ni want to treat you right\nso you wont ever want to leave`,
+    },
+    {
+        date: '6/4/2024',
+        haiku: `stalk me any day\ni'm all yours anyway`,
+    },
+    {
+        date: '6/5/2024',
+        haiku: `we exchanged our childhood movies\ni can't wait to watch all of them\ni want to know you so deeply\nthat i can read your mind and live in it`,
+    },
+    {
+        date: '6/6/2024',
+        haiku: `if you're unable to believe\nthat you deserve everything good and beatiful\ni'll give them to you anyway\nlet me show you how much\nof a dirty liar your brain can be`,
+    },
+    {
+        date: '6/7/2024',
+        haiku: `this day is for waiting\nfor thinking about you\neven more than usual\nfor counting how many more hours\nuntil i can have you again`,
+    },
+    {
+        date: '6/8/2024',
+        haiku: `beautiful girl under beatiful fireworks\nwhat a beautiful night\nwhy does time go faster when i'm with you?\nwhy can't it freeze for a while\nso i can savor you, us\nso i could get lost in the moment\nthe way we got a little lost in that maze of a mall`,
+    },
+    {
+        date: '6/9/2024',
+        haiku: `we were under cold air\nafter cold ice cream\nyet i was burning\nunder warm sheets\nour warm bodies\ncatching my breath\nin the crook of your neck\n\nthe morning came too fast`,
+        isSpg: true
+    },
+    {
+        date: '6/10/2024',
+        haiku: `it's a long road to walk\ni'll walk it with you\nand help carry your bags\nif you get tired, you can rest beside me\n`,
+    },
+    {
+        date: '6/11/2024',
+        haiku: `in the restaurant i thought about it\nin public transpo i though about it\nwhile walking i thought about it\nwhen i got home i thought about it\nit's not good to be this undone by you\nbut i can't help it`,
+        isSpg: true
+    },
+    {
+        date: '6/12/2024',
+        haiku: `here's to a month of stories with you\nand to so many more\nhappy monthsary\ni love you`,
+    },
+];
